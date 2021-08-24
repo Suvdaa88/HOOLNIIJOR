5d4ea1f1536bfd4cb119d3166a1eeb0341341abd
Suvdaa <hertlauriddle88@gmail.com>
2021-08-24 22:29:33 +0800

    Duuslaa

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/js/index.js",
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "docs")
   },
-  devtool: "inline-source-map",
   devServer: {
     contentBase: "./docs"
   },
